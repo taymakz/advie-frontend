2e80009fd2257b1240f5829763586263f8b7723c
Taymaz <taymazak1382@Gmail.com>
2023-07-09 23:24:27 +0330

    added Transition Styles Back !

--- app.vue
+++ app.vue
@@ -42,45 +42,45 @@ favoriteStore.Init()
   </div>
 </template>
 
-<!-- <style> -->
-<!-- .page-enter-active { -->
-<!--  animation: page-fade-in 0.1s ease-out; -->
-<!-- } -->
+<style>
+.page-enter-active {
+  animation: page-fade-in 0.1s ease-out;
+}
 
-<!-- .page-leave-active { -->
-<!--  animation: page-fade-out 0.1s cubic-bezier(1, 0.5, 0.8, 1); -->
-<!-- } -->
+.page-leave-active {
+  animation: page-fade-out 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
 
-<!-- @keyframes page-fade-in { -->
-<!--  from { -->
-<!--    opacity: 0; -->
-<!--  } -->
-<!--  to { -->
-<!--    opacity: 1; -->
-<!--    transform: translateY(0); -->
-<!--  } -->
-<!-- } -->
+@keyframes page-fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 
-<!-- @keyframes page-fade-out { -->
-<!--  from { -->
-<!--    opacity: 1; -->
-<!--    transform: translateY(0); -->
-<!--  } -->
-<!--  to { -->
-<!--    opacity: 0; -->
-<!--  } -->
-<!-- } -->
+@keyframes page-fade-out {
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+  }
+}
 
-<!-- .layout-enter-active { -->
-<!--  transition: all 0.1s ease-out; -->
-<!-- } -->
+.layout-enter-active {
+  transition: all 0.1s ease-out;
+}
 
-<!-- .layout-leave-active { -->
-<!--  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1); -->
-<!-- } -->
+.layout-leave-active {
+  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
 
-<!-- .layout-enter-from, -->
-<!-- .layout-leave-to { -->
-<!--  opacity: 0; -->
-<!-- } -->
-<!-- </style> -->
+.layout-enter-from,
+.layout-leave-to {
+  opacity: 0;
+}
+</style>
